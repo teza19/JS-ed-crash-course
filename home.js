9c6f8f76a117800f77cd4cbe08eaa0809d1c347b
teza19 <131840403+teza19@users.noreply.github.com>
2023-08-11 22:24:01 -0500

    Add files via upload

---
+++ home.js
@@ -0,0 +1,36 @@
+console.log("hello");
+console.log("this the javascript");
+//alert("Yoooo!");
+
+for (a=1; a<10; a++) {
+  console.log(a);
+}
+
+
+//data types
+
+
+var i = 100;
+console.log(i);
+
+var i = "hello";
+console.log(i);
+console.log(typeof(i));
+
+var name;
+console.log(name);
+
+var c = [1,2,3,4,5,6,7,8,9,10];
+console.log(c);
+
+console.log(c [0]);
+
+console.log(c.length);
+
+var d = new Array(3,5,6);
+
+console.log(d);
+
+let f = c+d[2];
+
+console.log(f);
